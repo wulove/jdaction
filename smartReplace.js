6bd223599fa6ecd5c71d223bc90a45ea7bca2807
wulove <iwulove@gmail.com>
2021-03-08 17:54:50 +0800

    update

--- smartReplace.js
+++ smartReplace.js
@@ -162,8 +162,7 @@ function batchReplace() {
 //#region 文件下载
 
 async function downloader_jd() {
-    if (/require\(['"`]{1}.\/jdCookie.js['"`]{1}\)/.test(remoteContent))
-        await download("https://github.com/wulove/jd_scripts/raw/master/jdCookie.js", "./jdCookie.js", "京东Cookies");
+    await download("https://github.com/wulove/jd_scripts/raw/master/jdCookie.js", "./jdCookie.js", "京东Cookies");
     if (remoteContent.indexOf("jdFruitShareCodes") > 0) {
         await download(
             "https://github.com/wulove/jd_scripts/raw/master/jdFruitShareCodes.js",
